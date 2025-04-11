cbe38c6c3a9156d4a5f2a79ebe6fe431e6804197
Nikonov Nikita <niki2ta00n4@gmail.com>
2025-04-11 20:09:40 +0300

    8am 11.04

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 
-import './App.css'
-
-function App() {
+import { TrpcProvaider } from './lib/trpc'
+const ideas = [
+  {
+      id: 1, name: 'text1', description: 'this is description with idea!'
+  },
+  {
+      id: 2, name: 'text2', description: 'this is description with idea!'
+  },
+  {
+      id: 3, name: 'text3', description: 'this is description with idea!'
+  },
+  {
+      id: 4, name: 'text4', description: 'this is description with idea!'
+  },
+  {
+      id: 5, name: 'text5', description: 'this is description with idea!'
+  },
+]
+export function Data() {
 
 
   return (
-    <>
-
-    </>
+    <div>
+{ideas.map(item=>(
+<div key={`${item}--${item.id}`}>
+   <div>{item.name}</div>
+  <div>{item.description}</div>
+</div> 
+))}
+    </div>
   )
 }
 
-export default App
+ export const App = () =>{
+  return (
+    <TrpcProvaider>
+<Data/>
+    </TrpcProvaider>
+  )
+}
--- src/lib/trpc.tsx
+++ src/lib/trpc.tsx
@@ -1,5 +1,32 @@
-import type {TrcpRouter} from '@app/'
-import { createTRPCReact } from '@trpc/react-query';
+import type {TrpcRouter} from '@app/backend/src/trpc'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createTRPCReact, httpBatchLink } from '@trpc/react-query';
 
 
 const trpc = createTRPCReact<TrpcRouter>()
+
+const queryClient = new QueryClient({
+defaultOptions: {
+    queries: {
+        retry: false,
+        refetchOnWindowFocus: false
+    }
+}
+})
+
+const trpcClient = trpc.createClient({
+    links:[
+httpBatchLink({
+    url: 'http://localhost:5433/trpc'
+})
+    ]
+})
+
+
+export const TrpcProvaider = ({children}: {children: React.ReactNode}) => {
+    return (
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        </trpc.Provider>
+    )
+}
